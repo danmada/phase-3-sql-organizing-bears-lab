750a3a05c9238245dbffc09e205ce260c26ce988
danmada <danmada@gmail.com>
2021-07-22 17:40:40 -0500

    commit tree f138738f64cc1ae6073d030f893b1758c61486b1

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Chocolate", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Red", "Chill", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Black", "Normal", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 16, "F", "Grey", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 8, "F", "Brown", "Mean", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Black", "Nice", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F", "White", "Chill", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 10, "M", "Red", "Angry", false);
